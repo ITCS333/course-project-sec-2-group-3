b6b10f87f1dc094fbd1b1e591218e36bc8d19566
Abrar87D <abrar.hassan.hilal@gmail.com>
2026-05-21 19:52:37 +0300

    update db e

--- src/admin/api/index.php
+++ src/admin/api/index.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 
 // TODO: Include the database connection file.
-require_once "/../../common/db.php";
+require_once "db.php";
 
 // Assume a function getDBConnection() is available that returns a PDO instance
 // configured for the 'course' database (see schema.sql).
